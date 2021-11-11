c26ee154246cf0feb3f315f0a10e2c83958f51ef
Nick Stebb <nickolarse@riseup.net>
2021-11-11 16:11:51 +1300

    add Space selectors

--- client/src/features/space/slice.ts
+++ client/src/features/space/slice.ts
@@ -1,23 +1,31 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Dictionary, TimeFrame } from "app/types";
+import { RootState } from "app/store";
+
+export const getThisWeekSpaces = (state: RootState) => {
+  return state?.space.thisWeek;
+};
+export const getLastWeekSpaces = (state: RootState) => {
+  return state?.space.lastWeek;
+};
 
 export interface Space {
   timeframe: TimeFrame;
 }
 
-export interface NewSpacePayload {
-  spaceId: string;
-  space: Space;
-}
+// export interface NewSpacePayload {
+//   spaceId: string;
+//   space: Space;
+// }
 
-export interface DeleteSpacePayload {
-  spaceId: string;
-}
+// export interface DeleteSpacePayload {
+//   spaceId: string;
+// }
 
-export interface UpdateSpacePayload {
-  spaceId: string;
-  spacePatch: Partial<Space>;
-}
+// export interface UpdateSpacePayload {
+//   spaceId: string;
+//   spacePatch: Partial<Space>;
+// }
 // import merge from "deepmerge";
 import luxon from "luxon";
 
